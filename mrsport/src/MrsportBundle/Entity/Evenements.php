38c3b7e61445e0f5f48537354f34faf29af87c81
Anais Van den Driessche <anaisvdd@gmail.com>
2018-06-07 12:53:45 +0200

    modif page evenement

--- mrsport/src/MrsportBundle/Entity/Evenements.php
+++ mrsport/src/MrsportBundle/Entity/Evenements.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="evenements")
  * @ORM\Entity(repositoryClass="MrsportBundle\Repository\evenementsRepository")
  */
-class evenements
+class Evenements
 {
     /**
      * @var int
